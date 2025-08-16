9fcdfb381b96cb69602f9ff7748f2d10ab1e5ca1
KudoroEsther <kudoroesther.learn@gmail.com>
2025-08-16 23:46:53 +0100

    Add files via upload
    Completed task 2

--- Week_2_Tasks/Kudoro_Esther_Task7_Dictionary/Task7.py
+++ Week_2_Tasks/Kudoro_Esther_Task7_Dictionary/Task7.py
@@ -1,38 +1,35 @@
-#Task 1 Student biodata storage
-info = {
-    "Name": input("Input your name: "),
-    "age": int(input("Enter your age: ")),
-    "gender": input("Input your gender: "),
-    "course": input("Enter your course: ").split()
-}
-# print(*info.items(), sep="\n")
-for key, val in info.items():
-    print(f"{key}:\t{val}") #prints the content of the dictionary on a new line with tab in between
-
-# #Task 2 Super market price list
-# # items_price = {}
-# item = ["rice", "beans", "banana", "mango"]
-# i=0
-# prices = []
-# # prices = input(f"Enter the price for each item: ")
-# for i in item:
-#     price = int(input(f"Enter the price for {i}: "))
-#     prices.append(price)
-#     # items_price[f"{item}"] = price
-# items_price = zip(item,prices)
-#     # itemss = {item:prices}
-# print(items_price)
-# print(prices)
-# print(f"{item}:{prices}")
-
-# items_price = {item, price}
-# print(items_price)
+# #Task 1 Student biodata storage
+# info = {
+#     "Name": input("Input your name: "),
+#     "age": int(input("Enter your age: ")),
+#     "gender": input("Input your gender: "),
+#     "course": input("Enter your course: ").split()
+# }
+# # print(*info.items(), sep="\n")
+# for key, val in info.items():
+#     print(f"{key}:\t{val}") #prints the content of the dictionary on a new line with tab in between
 
 
-#create a list with items
-#creat a variable that accepts integer inputs for the price
-#add list and price to a dict known as list_price
+#Task 2 Super market price list
+items_price = {} #defined an empty dictionary
+items = ["rice", "beans", "banana"]
+items_price["rice"] = float(input(f"Enter the price for {items[0]}: ")) #this accept float input and assigns it to the key rice, indexing is used to display the appropriate item
+items_price["beans"] = float(input(f"Enter the price for {items[1]}: "))
+items_price["banana"] = float(input(f"Enter the price for {items[2]}: "))
 
-# item = ["rice", "beans", "banana", "mango"]
+print(items_price.keys())
+print(type(items_price))
+updated_item = input("Input the name of the item to be updated: ").lower() #this accepts the name of the item to be updated and .lower() is used to prevent error in the next line
+if updated_item in items:
+    updated_price = float(input(f"Input the new price of the {updated_item}: ")) #if the inputted item is found in items, it accepts a new price
+    items_price.update({updated_item:updated_price}) #this updates the price in the main dictionary
+else:
+    print("Invalid item, try again")
+print(items_price) #this prints the updated dictionary
 
-# price = int(input(f"Enter the price for {item}"))
+
+#Task 4 Days and acitvities pairing
+week = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+act1 = input(f"Input an activity for {week[0]}: ")
+act2 = input(f"Input an activity for {week[1]}: ")
+act3 = input(f"Input an activity for {week[2]}: ")
